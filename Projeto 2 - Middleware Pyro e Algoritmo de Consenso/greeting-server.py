5201e723cfc6d9a64673bbb1c80faa56e522f62e
Taresu <thales-ss@live.com>
2024-05-01 19:23:19 -0300

    Create client basic files

--- Projeto 2 - Middleware Pyro e Algoritmo de Consenso/greeting-server.py
+++ Projeto 2 - Middleware Pyro e Algoritmo de Consenso/greeting-server.py
@@ -1,11 +1,12 @@
 # saved as greeting-server.py
 import Pyro5.api
 
+
 @Pyro5.api.expose
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hello, {0}. Here is your fortune message:\n" \
-               "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
+            "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
 
 daemon = Pyro5.api.Daemon()             # make a Pyro daemon
 uri = daemon.register(GreetingMaker)    # register the greeting maker as a Pyro object
