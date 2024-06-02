0695feb5cc8048049bdbd67fd0cc2fb3f420e4df
Herm4nnJR <luishermann@hotmail.com>
2024-06-02 11:29:20 -0300

    Deiando o js mais legivel

--- Projeto 3 - Serviços Web (REST ou gRPC)/Client/js/script.js
+++ Projeto 3 - Serviços Web (REST ou gRPC)/Client/js/script.js
@@ -12,40 +12,39 @@ document.addEventListener('DOMContentLoaded', () => {
         const editora = document.getElementById('editora').value;
 
         var urlFunction = url + '/livros';
-
-        fetch(urlFunction, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ titulo, autor, editora })
-        })
-        .then(response => response.json())
-        .then(data => {
+        try{
+            const response = fetch(urlFunction, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ titulo, autor, editora })
+            });
+            const data = response.json();
             if (data.mensagem === 'Livro cadastrado com sucesso!') {
                 alert(data.mensagem);
                 carregarLivros(); // Atualiza a lista de livros
             } else {
                 alert('Erro: ' + data.mensagem);
             }
-        })
-        .catch(error => console.error('Erro:', error));
+        }
+        catch(error) {console.error('Erro Cadastrar Livros:', error);}
     });
 
     async function carregarLivros() {
 
         var urlFunction = url + '/livros';
-        fetch(urlFunction)
-            .then(response => response.json())
-            .then(data => {
-                listaLivros.innerHTML = '';
-                data.forEach(livro => {
-                    const li = document.createElement('li');
-                    li.textContent = `${livro.titulo} - ${livro.autor} (${livro.editora})`;
-                    listaLivros.appendChild(li);
-                });
+        try {
+            const response = await fetch(urlFunction);
+            const data = await response.json();
+            listaLivros.innerHTML = '';
+            data.forEach(livro => {
+                const li = document.createElement('li');
+                li.textContent = `${livro.titulo} - ${livro.autor} (${livro.editora})`;
+                listaLivros.appendChild(li);
             })
-            .catch(error => console.error('Erro carregar Livros:', error));
+        }
+        catch(error) {console.error('Erro carregar Livros:', error)};
     }
 
     // Carrega os livros ao iniciar a página
